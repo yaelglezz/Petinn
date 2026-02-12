97191c60b21c051fa6c02bc6c285ea005bfa807d
yaelglezz <yaelglez902@gmail.com>
2026-02-12 16:17:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 const express = require('express');
-const app = express();
+const fetch = require('node-fetch');
 
+const app = express();
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
-const verifyToken = process.env.VERIFY_TOKEN;
+const scriptURL = "https://script.google.com/macros/s/AKfycby_ZHcAVqxSReeGSLPGZLUrWr85HlurKRU670Sl0VW-wE55zKW7qmd2J73cmuN8kFzfSA/exec";
 
-app.get('/', (req, res) => {
-  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
+app.post('/', async (req, res) => {
 
-  if (mode === 'subscribe' && token === verifyToken) {
-    console.log('WEBHOOK VERIFIED');
-    res.status(200).send(challenge);
-  } else {
-    res.status(403).end();
-  }
-});
-
-app.post('/', (req, res) => {
-  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-  console.log(`\n\nWebhook received ${timestamp}\n`);
+  console.log("Webhook recibido:");
   console.log(JSON.stringify(req.body, null, 2));
-  res.status(200).end();
+
+  try {
+
+    const mensaje =
+      req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0]?.text?.body || "";
+
+    const numero =
+      req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0]?.from || "";
+
+    console.log("Numero:", numero);
+    console.log("Mensaje:", mensaje);
+
+    await fetch(scriptURL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        numero: numero,
+        mensaje: mensaje
+      }),
+    });
+
+    console.log("Enviado a Google Script");
+
+  } catch (error) {
+    console.error("Error enviando a Script:", error);
+  }
+
+  res.sendStatus(200);
 });
 
-app.listen(port, () => {
-  console.log(`\nListening on port ${port}\n`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor activo en puerto ${PORT}`);
 });
